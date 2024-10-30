5ece581c7eb88b5f8adc8cac97128b6ec9d9ff21
HugoDesb <hugo.desbiolles@gmail.com>
2024-10-30 18:54:24 +0100

    fix setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,6 +124,7 @@ dependencies {
     }
     implementation(kotlin("stdlib-jdk8"))
     testImplementation(libs.junit)
+    implementation(kotlin("script-runtime"))
 }
 
 // ------------------------------------------------------------------------------------------------------------------ //
@@ -153,7 +154,6 @@ tasks {
             attributes["Implementation-Version"] = project.version
         }
         minimize {
-            exclude(dependency("org.openrndr:openrndr-gl3:.*"))
             exclude(dependency("org.jetbrains.kotlin:kotlin-reflect:.*"))
             exclude(dependency("org.slf4j:slf4j-simple:.*"))
             exclude(dependency("org.apache.logging.log4j:log4j-slf4j2-impl:.*"))
@@ -251,8 +251,8 @@ class Openrndr {
 
     init {
         dependencies {
-            runtimeOnly(openrndr("gl3"))
-            runtimeOnly(openrndrNatives("gl3"))
+            implementation(openrndr("gl3"))
+            implementation(openrndrNatives("gl3"))
             implementation(openrndr("openal"))
             runtimeOnly(openrndrNatives("openal"))
             implementation(openrndr("application"))
